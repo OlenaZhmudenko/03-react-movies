a04d5041e50fca7b11c199f817f6ee61ea319f3a
alyonka <e.zhmudenko@gmail.com>
2025-09-06 23:53:49 -0400

    second commit

---
+++ src/services/movieService.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import type { Movie, MoviesResponse } from '../types/movie';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+if (!TOKEN) {
+    throw new Error('VITE_TMDB_TOKEN  environment variable is not set');
+}
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        Authorization: `Bearer ${TOKEN}`,
+    },
+});
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+    try {
+        const response = await api.get<MoviesResponse>('/search/movie', {
+            params: {
+                query,
+                include_adult: false,
+                language: 'en-US',
+                page: 1,
+            },
+        });
+        return response.data.results;
+    } catch (error) {
+        console.error('Failed to fetch movies:', error);
+        throw error;
+    }
+};
